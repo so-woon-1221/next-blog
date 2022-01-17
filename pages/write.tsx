cb775c3edf54489da556b2e90a9e39db09452c1f
so-woon-1221 <janus7359@gmail.com>
2022-01-18 00:14:24 +0900

    에디터는 ssr 아님

--- pages/write.tsx
+++ pages/write.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import dynamic from "next/dynamic";
 
-const Editor = dynamic(() => import("../components/Editor"));
+const Editor = dynamic(() => import("../components/Editor"), { ssr: false });
 
 const write: React.FC = () => {
-  const isSSR = typeof window === "undefined";
   return (
     <div className="w-full h-full">
       <h1>글쓰기</h1>
-      {!isSSR && (
-        <React.Suspense fallback={() => <div>a</div>}>
-          <Editor />
-        </React.Suspense>
-      )}
+
+      <Editor />y
     </div>
   );
 };

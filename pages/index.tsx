71865a977434eba26aea559b8af6c9abb73c0f19
so-woon-1221 <janus7359@gmail.com>
2022-02-13 21:07:14 +0900

    다시 추가해볼게.. 도와줘 친구들

---
+++ pages/index.tsx
@@ -0,0 +1,80 @@
+import axios from "axios";
+import { GetStaticProps } from "next";
+import React from "react";
+import Link from "next/link";
+
+const development = process.env.NODE_ENV !== "production";
+
+const index = ({ posts }: { posts: any }) => {
+  console.log(posts);
+
+  return (
+    <div className="divide-y">
+      {posts.map(
+        (
+          d: {
+            name: string;
+            data: {
+              content: string;
+              data: any;
+              excerpt: string;
+              isEmpty: boolean;
+              orig: any;
+            };
+          },
+          i: number
+        ) => {
+          return (
+            <Link href={`/post/${d.name}`} passHref key={`post-${i}`}>
+              <div className="cursor-pointer space-y-2 px-4 py-8 rounded">
+                <div className="flex">
+                  <h2 className="font-bold flex-grow text-xl">
+                    {d.data?.data?.title}
+                  </h2>
+                  <div className="text-sm text-gray-500">
+                    {d.data?.data?.date?.substr(0, 10)}
+                  </div>
+                </div>
+
+                <div className="flex space-x-2">
+                  {d.data?.data?.category
+                    .split(",")
+                    .map((d: string, i: number) => {
+                      return (
+                        <Link
+                          href={`/tag/${d.trim()}`}
+                          passHref
+                          key={`post-${i}-category-${i}`}
+                        >
+                          <div className="bg-zinc-100 dark:bg-zinc-900 px-2 py-0.5 rounded-lg hover:bg-zinc-300 dark:hover:bg-zinc-700">
+                            # {d.trim()}
+                          </div>
+                        </Link>
+                      );
+                    })}
+                </div>
+              </div>
+            </Link>
+          );
+        }
+      )}
+    </div>
+  );
+};
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await axios.get(
+    `${
+      development ? "http://localhost:3000" : "https://sowoon-1221.vercel.app"
+    }/api/posts`
+  );
+
+  const posts = response.data;
+
+  return {
+    props: {
+      posts: posts.data,
+    },
+  };
+};
+
+export default index;
---
+++ pages/post/[title].tsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import React from "react";
+import Head from "next/head";
+import { marked } from "marked";
+
+const development = process.env.NODE_ENV !== "production";
+
+const Title = ({ params, data }: any) => {
+  // 그 h 태그 따라서 가는 거 만들어야함
+  // 클릭 네비게이터
+  return (
+    <div className="prose prose-slate px-4">
+      <Head>
+        <title>{params.title}</title>
+      </Head>
+      <div dangerouslySetInnerHTML={{ __html: marked(data.content) }}></div>
+    </div>
+  );
+};
+
+export const getStaticPaths = async () => {
+  const response = await axios.get(
+    `${
+      development ? "http://localhost:3000" : "https://sowoon-1221.vercel.app"
+    }/api/posts`
+  );
+  const posts = response.data;
+
+  const paths = posts.data.map((d: any) => ({
+    params: {
+      title: d.name,
+    },
+  }));
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async ({ params }: any) => {
+  const response = await axios.get(
+    encodeURI(
+      `${
+        development ? "http://localhost:3000" : "https://sowoon-1221.vercel.app"
+      }/api/post?title=${params.title}`
+    )
+  );
+
+  const post = response.data;
+  return { props: { params, data: post.data } };
+};
+
+export default Title;

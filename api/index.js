4eeaef94322f9a32edc1efa1227106b657fe024a
so-woon-1221 <janus7359@gmail.com>
2022-02-13 23:13:02 +0900

    api test

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
-const app = require("express");
+const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+const app = express();
+
 app.post("/api/write", (req, res) => {
-  return res.json("asdfa");
+  const { title, post } = req.body;
+  // const path = join(process.cwd(), "posts");
+  const file = fs.writeFileSync(
+    path.join(__dirname, "posts", `${title.replace(/ /g, "")}.md`),
+    post
+  );
+
+  return res.json("저장완료");
 });
 
 module.exports = app;

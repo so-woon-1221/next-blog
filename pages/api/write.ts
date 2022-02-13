7039d6c37032163102b667c52bf60b469204ca98
so-woon-1221 <janus7359@gmail.com>
2022-02-13 23:17:53 +0900

    api 변경...

---
+++ pages/api/write.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
+import { join } from "path";
+// import matter from "gray-matter";
+
+const writePost = (req: NextApiRequest, res: NextApiResponse) => {
+  const { title, post } = req.body;
+  const path = join(process.cwd(), 'posts')
+  const file = fs.writeFileSync(
+    join(__dirname  + `/posts`, `${title.replace(/ /g, "")}.md`),
+    post
+  );
+
+  return res.json("저장완료");
+};
+
+export default writePost;
